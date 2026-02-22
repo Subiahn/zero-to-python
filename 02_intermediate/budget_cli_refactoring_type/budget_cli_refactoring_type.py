fbfd30ce606a2adea76d7d0b534a4f5c3827ee45
Subi <subi@Subiui-MacBookPro.local>
2026-02-22 14:58:45 +0900

    feat: 타입 힌트 추가 budget_cli_refactoring_type

---
+++ 02_intermediate/budget_cli_refactoring_type/budget_cli_refactoring_type.py
@@ -0,0 +1,31 @@
+
+
+class BudgetApp:
+    def __init__(self) -> None:
+        self.transacrions = [] # 거래 내역 리스트
+
+    def add_income(self, amount: int, memo: str) -> None:
+        self.transacrions.append({"type": "수입", "amount": amount, "memo": memo})
+
+
+    def add_expense(self, amount: int, memo: str) -> None:
+        self.transacrions.append({"type": "지출", "amount": amount, "memo": memo})
+
+    def show_summary(self) -> None:
+        income = sum(r['amount'] for r in self.transacrions if r["type"] == "수입")
+        expense = sum(r['amount'] for r in self.transacrions if r["type"] == "지출")
+        print(f"수입: {income}, 지출: {expense}, 잔액: {income - expense}")
+
+    def show_all(self) -> None:
+        for t in self.transacrions:
+            print(f"{t['type']} | {t['memo']} | {t['amount']}원")
+
+app = BudgetApp()
+
+# 예제 코드
+app.add_income(50000, "알바비")
+app.add_income(30000, "용돈")
+app.add_expense(12000, "점심")
+app.add_expense(3500, "커피")
+app.show_all()
+app.show_summary()
